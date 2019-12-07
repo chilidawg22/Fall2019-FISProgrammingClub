1811b14f6215737847e1536d28989ebf014a6e4d
Michael B <mcb2022@gmail.com>
2019-12-07 15:24:41 +0100

    ball throwing

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,22 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Valve.VR;
 
 public class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
+
+    public SteamVR_Input_Sources player;
+    public GameObject ball;
+    Vector3 position = new Vector3(1,1,1); 
+    Quaternion rotation = new Quaternion(0,0,0);
+
 
     // Update is called once per frame
     void Update()
     {
-        /*if (Input.GetButtonDown("TestTrigger")){
-            
-        }*/
+        if (SteamVR_Actions._default.TestTrigger.GetStateDown(player)){
+            Instantiate(ball, position, rotation );
+        }
 
     }
 }
