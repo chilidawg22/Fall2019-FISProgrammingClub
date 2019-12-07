d9d792302907ab92407bd0958ab0484c41fcd4dc
Michael B <mcb2022@gmail.com>
2019-12-07 15:03:58 +0100

    coding

--- Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -105,6 +105,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Boolean TestTrigger
+        {
+            get
+            {
+                return SteamVR_Actions.default_TestTrigger;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
