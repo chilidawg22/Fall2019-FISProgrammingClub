6e2b1dc20c9b3e2af4654c5a767982cdaef0922c
Michael B <mcb2022@gmail.com>
2019-12-07 16:11:19 +0100

    Add raycast indicator

---
+++ Assets/Scripts/RaycastMaster.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastMaster : MonoBehaviour
+{
+
+    public GameObject indicator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate() {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.eulerAngles, out hit)) {
+            indicator.transform.position = hit.point;
+        }
+    }
+}
