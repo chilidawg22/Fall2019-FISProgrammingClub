b380d3162922211a6e5d9a2dca1a918fc3fcf5d5
OctoMeep <roboneleven@gmail.com>
2019-12-07 17:32:11 +0100

    Check whether raycast hits target

--- Assets/Scripts/RaycastMaster.cs
+++ Assets/Scripts/RaycastMaster.cs
@@ -6,15 +6,23 @@ public class RaycastMaster : MonoBehaviour
 {
 
     public GameObject indicator;
+    public GameObject ball;
 
   
     void FixedUpdate() {
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, Vector3.forward + transform.eulerAngles /*transform.eulerAngles*/, out hit)) {
+        if (Physics.Raycast(transform.position, transform.forward /*transform.eulerAngles*/, out hit)) {
             indicator.transform.position = hit.point;
-            Debug.Log(hit.point.x + ", " + hit.point.y + ", " + hit.point.z);
+            //Debug.Log(hit.point.x + ", " + hit.point.y + ", " + hit.point.z);
 
-            Debug.Log("indicator " + indicator.transform.position.x + ", " + indicator.transform.position.y + ", " + indicator.transform.position.z);
+            //Debug.Log("indicator " + indicator.transform.position.x + ", " + indicator.transform.position.y + ", " + indicator.transform.position.z);
+            Collider hitC = hit.collider;
+            Collider ballC = ball.GetComponent<Collider>();
+            Debug.Log(hitC);
+            Debug.Log(ballC);
+            if (hitC == ballC) {
+                Debug.Log("hit!");
+            }
         }
     }
 }
