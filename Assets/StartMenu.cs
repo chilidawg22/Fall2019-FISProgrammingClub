ce402fedec7598d1567b8124430d1306123655e1
TimothyGorst <32100187+TimothyGorst@users.noreply.github.com>
2019-12-08 17:43:50 +0100

    StartMenu

---
+++ Assets/StartMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+public SteamVR_Input_Sources player;
+
+
+    void Update()
+    {
+        if (SteamVR_Actions._default.TestTrigger.GetStateDown(player)){
+            SceneManager.LoadScene("Level1");
+        }
+
+    }
+}
