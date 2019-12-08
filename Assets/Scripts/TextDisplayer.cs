773611164c5720e70ca7adcfba1f0f17ec772ccf
TimothyGorst <32100187+TimothyGorst@users.noreply.github.com>
2019-12-08 15:08:13 +0100

    Stuff

---
+++ Assets/Scripts/TextDisplayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TextMeshProUGUI
+public class TextDisplayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private TextMeshProUGUI Score;
+    
+    void Start()
+    {
+        Score = getComponent<TextMeshProUGUI> ();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Score.text = .toString(timeOnTarget);
+    }
+}
